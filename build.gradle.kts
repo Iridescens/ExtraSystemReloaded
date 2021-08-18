9814f2c1d2e8f831293ff2174824e34fc441212c
scardwell15 <scardwell15@gmail.com>
2021-08-17 22:51:23 -0400

    [0.7.4] upped version to 0.7.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 object Variables {
     // Note: On Linux, if you installed Starsector into ~/something, you have to write /home/<user>/ instead of ~/
     val starsectorDirectory = "C:/Games/Starsector"
-    val modVersion = "0.7.2"
+    val modVersion = "0.7.4"
     val jarFileName = "ExtraSystemCore.jar"
 
     val modId = "extra_system_reloaded"
